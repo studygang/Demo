e376fb33ed6f7bfb437d9ef7f7dbbb8a2bf00764
gangzi <2237444125@qq.com>
2017-04-13 15:00:11 +0800

    base

---
+++ app/src/main/java/com/gangzi/demo/base/BaseFragment.java
@@ -0,0 +1,41 @@
+package com.gangzi.demo.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by gangzi on 2017/4/13.
+ */
+
+public abstract class BaseFragment extends Fragment{
+
+    private Context mContext;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext=getActivity();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return initView();
+    }
+
+    public abstract View initView();
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    private void initData() {
+    }
+}
