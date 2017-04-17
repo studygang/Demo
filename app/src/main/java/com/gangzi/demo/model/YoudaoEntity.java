3e54ac054af9f946e50c2d991326515b40501891
gangzi <2237444125@qq.com>
2017-04-17 11:39:50 +0800

    有道词典API的接入

---
+++ app/src/main/java/com/gangzi/demo/model/YoudaoEntity.java
@@ -0,0 +1,116 @@
+package com.gangzi.demo.model;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/4/14.
+ */
+
+public class YoudaoEntity {
+
+    /**
+     * translation : ["we"]
+     * basic : {"phonetic":"wǒ men","explains":["we","us","ourselves"]}
+     * query : 我们
+     * errorCode : 0
+     * web : [{"value":["We","US","About us"],"key":"我们"},{"value":["Contact Us","CONTACT US Contact Us","Message"],"key":"联系我们"},{"value":["Contact us","CONTACT US Contact Us","Forum"],"key":"联络我们"}]
+     */
+
+    private BasicBean basic;
+    private String query;
+    private int errorCode;
+    private List<String> translation;
+    private List<WebBean> web;
+
+    public BasicBean getBasic() {
+        return basic;
+    }
+
+    public void setBasic(BasicBean basic) {
+        this.basic = basic;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public List<String> getTranslation() {
+        return translation;
+    }
+
+    public void setTranslation(List<String> translation) {
+        this.translation = translation;
+    }
+
+    public List<WebBean> getWeb() {
+        return web;
+    }
+
+    public void setWeb(List<WebBean> web) {
+        this.web = web;
+    }
+
+    public static class BasicBean {
+        /**
+         * phonetic : wǒ men
+         * explains : ["we","us","ourselves"]
+         */
+
+        private String phonetic;
+        private List<String> explains;
+
+        public String getPhonetic() {
+            return phonetic;
+        }
+
+        public void setPhonetic(String phonetic) {
+            this.phonetic = phonetic;
+        }
+
+        public List<String> getExplains() {
+            return explains;
+        }
+
+        public void setExplains(List<String> explains) {
+            this.explains = explains;
+        }
+    }
+
+    public static class WebBean {
+        /**
+         * value : ["We","US","About us"]
+         * key : 我们
+         */
+
+        private String key;
+        private List<String> value;
+
+        public String getKey() {
+            return key;
+        }
+
+        public void setKey(String key) {
+            this.key = key;
+        }
+
+        public List<String> getValue() {
+            return value;
+        }
+
+        public void setValue(List<String> value) {
+            this.value = value;
+        }
+    }
+}
